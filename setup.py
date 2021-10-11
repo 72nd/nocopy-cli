3c9e4af6002ad22bc03de668bf1a513d13c9f8cc
72nd <msg@frg72.com>
2021-10-11 12:49:40 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nocopy-cli",
+    version="0.1.0",
+    author="72nd",
+    author_email="msg@frg72.com",
+    description="CLI client application for NocoDB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/72nd/nocopy-cli",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    entry_points="""
+        [console_scripts]
+        nocopy=nocopy.cli:cli
+    """,
+    install_requires=[
+        "click==8.0.1",
+        "pydantic==1.8.2",
+        "python-Levenshtein==0.12.0",
+        "nocopy==0.1.5",
+        "thefuzz==0.19.0",
+    ]
+)
